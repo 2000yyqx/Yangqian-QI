035fcb73def5a89ef79384f87b163471d486c40e
2000yyqx <96576653+2000yyqx@users.noreply.github.com>
2023-07-02 04:52:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import streamlit as st
 from streamlit_folium import folium_static
 st.title('重庆旅游简易导览（下方为景点热力图）')
 # 读取CSV文件
-locations_df = pd.read_csv(r'C:\Users\ASUS\Desktop\软件工程与GIS开发\CQ.csv')
+url="https://github.com/2000yyqx/Yangqian-QI/blob/main/CQ.csv"
+locations_df = pd.read_csv(url,encoding='utf-8')
+#locations_df = pd.read_csv(r'C:\Users\ASUS\Desktop\软件工程与GIS开发\CQ.csv')
 
 # 创建地图
 m = folium.Map(location=[29.5636, 106.5516], zoom_start=12)
